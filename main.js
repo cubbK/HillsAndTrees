779acb3e5e79457b75b0d898b5b2057d1904dd25
Dan <danpopa1998@gmail.com>
2016-12-19 01:32:04 +0200

    3 munti se deseneaza in cool sur

--- main.js
+++ main.js
@@ -8,15 +8,17 @@ function randomRange(min, max) {
 }
 //End Helpers
 
-const canvasWidth = 768;
+const canvasWidth = 1400;
 const canvasHeight = 480;
 
 class Hill {
-  constructor(detail, height, rangeX, rangeY) {
+  constructor(detail, height, rangeX, rangeY, color, posY) {
     this.detail = detail;
     this.height = height;
     this.rangeX = canvasWidth / rangeX;
     this.rangeY = canvasHeight / rangeY;
+    this.color = color;
+    this.posY = posY;
   }
   getPath() {
     var path = [];
@@ -25,32 +27,34 @@ class Hill {
       if (path.length == 0) {
         newPath.push({
           'level': 0,
-          'value': randomRange(this.height - this.rangeY, this.height + this.rangeY)
+        'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) + this.posY * 70
         })
       } else {
         for (var i = 0; i < path.length; i++) {
           if (path[i].level == level - 1) {
             newPath.push({
               'level': level,
-              'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) / (level + 1)
+            'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) / (level + 1) + this.posY * 70
             });
             newPath.push(path[i]);
             newPath.push({
               'level': level,
-              'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) / (level + 1)
+            'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) / (level + 1) + this.posY * 70
             });
           } else {
-            newPath.push(path[i]);
+            newPath.push({
+              'level' : path[i].level,
+              'value' : path[i].value + this.posY *70
+            });
           }
         }
       }
       path = newPath;
     }
     var overrideRange = [Math.floor(path.length/2 -2) ,Math.floor(path.length/2 +2) ];
-    console.log(overrideRange);
     for (var i = overrideRange[0]; i <= overrideRange[1] ; i++) {
       if (i != overrideRange/2){
-        console.log(path[i]);
+       
         path[i]['value'] =randomRange(this.height - this.rangeY, this.height + this.rangeY) * 0.75;
       }
     }
@@ -58,13 +62,13 @@ class Hill {
 
   }
   draw(path) {
-    var triangle = new Konva.Shape({
+    var mountain = new Konva.Shape({
       sceneFunc: function(context) {
         context.beginPath();
         context.moveTo(0, canvasHeight);
         var sectorLength = canvasWidth / path.length;
         for (var i = 0; i < path.length; i++) {
-          context.lineTo(randomRange(sectorLength - 2, sectorLength + 2) * (i + 0.5), canvasHeight - path[i].value);
+          context.lineTo(randomRange(sectorLength - 3, sectorLength + 3) * (i + 0.5), canvasHeight - path[i].value);
         }
         context.lineTo(canvasWidth, canvasHeight);
         //context.quadraticCurveTo(150, 100, 260, 170);
@@ -73,13 +77,13 @@ class Hill {
         // Konva specific method
         context.fillStrokeShape(this);
       },
-      fill: '',
-      stroke: 'black',
+      fill: this.color,
+      stroke: this.color,
       strokeWidth: 5
     });
 
     // add the triangle shape to the layer
-    layer.add(triangle);
+    layer.add(mountain);
 
     // add the layer to the stage
     stage.add(layer);
@@ -96,6 +100,12 @@ var stage = new Konva.Stage({
 // then create layer
 var layer = new Konva.Layer();
 
-var hill = new Hill(4, 250, 30, 4);
-var path = hill.getPath();
-hill.draw(path);
+var colors = [ '#2a2a2a', '#3f3f3f', '#545454' ];
+
+for (let i = 2; i >= 0; i--){
+  let hill = new Hill(4, 250, 30, 4, colors[i], i);
+  let path = hill.getPath();
+  hill.draw(path);
+}
+
+
