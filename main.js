56729bf36393056b85f5e4e515f2bcefb245e414
Dan <danpopa1998@gmail.com>
2016-12-19 23:15:54 +0200

    se genereaza background frumos

--- main.js
+++ main.js
@@ -51,11 +51,12 @@ class Hill {
       }
       path = newPath;
     }
-    var overrideRange = [Math.floor(path.length/2 -2) ,Math.floor(path.length/2 +2) ];
+    //face ca in mijloc sa nu fie asa ascutit
+    var overrideRange = [Math.floor(path.length/2 -4) ,Math.floor(path.length/2 +4) ];
     for (var i = overrideRange[0]; i <= overrideRange[1] ; i++) {
       if (i != overrideRange/2){
        
-        path[i]['value'] =randomRange(this.height - this.rangeY, this.height + this.rangeY) * 0.75;
+        path[i]['value'] =randomRange(this.height - this.rangeY, this.height + this.rangeY) * 0.85;
       }
     }
     return path;
@@ -89,7 +90,24 @@ class Hill {
     stage.add(layer);
   }
 }
-
+function drawSky () {
+  var blueList = ['#6ca6cd' , '#9ac0cd', '#a9d4e1','#3a9dbc','#4a90ad'];
+  var randomList = ['#f5b049','#77bfa3', '#b7e3cc','#6c5f78','#6dd472' ];
+  var color1 = blueList[Math.floor(Math.random() * blueList.length)];
+  var color2 = randomList[Math.floor(Math.random() * randomList.length)];
+  var sky = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: canvasWidth,
+    height: canvasHeight,
+    fillLinearGradientStartPoint: { x : canvasWidth/2, y: canvasHeight},
+    fillLinearGradientEndPoint: { x : canvasWidth/2, y :0 },
+    fillLinearGradientColorStops: [0, color2, 1, color1],
+    strokeWidth: 4
+  });
+  layer.add(sky);
+  stage.add(layer);
+}
 // first we need to create a stage
 var stage = new Konva.Stage({
   container: 'canvas', // id of container <div>
@@ -102,10 +120,12 @@ var layer = new Konva.Layer();
 
 var colors = [ '#2a2a2a', '#3f3f3f', '#545454' ];
 
+drawSky();
 for (let i = 2; i >= 0; i--){
   let hill = new Hill(4, 250, 30, 4, colors[i], i);
   let path = hill.getPath();
   hill.draw(path);
 }
+
 
 
