eb6791d96989edb854d289da7ce48ea1a3e8e983
Dan <danpopa1998@gmail.com>
2016-12-22 18:39:16 +0200

    readme added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Helpers
-Array.prototype.insert = function(index, item) {
+Array.prototype.insert = function (index, item) {
   this.splice(index, 0, item);
 };
 
@@ -27,24 +27,24 @@ class Hill {
       if (path.length == 0) {
         newPath.push({
           'level': 0,
-        'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) + this.posY * 70
+          'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) + this.posY * 70
         })
       } else {
         for (var i = 0; i < path.length; i++) {
           if (path[i].level == level - 1) {
             newPath.push({
               'level': level,
-            'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) / (level + 1) + this.posY * 70
+              'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) / (level + 1) + this.posY * 70
             });
             newPath.push(path[i]);
             newPath.push({
               'level': level,
-            'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) / (level + 1) + this.posY * 70
+              'value': randomRange(this.height - this.rangeY, this.height + this.rangeY) / (level + 1) + this.posY * 70
             });
           } else {
             newPath.push({
-              'level' : path[i].level,
-              'value' : path[i].value + this.posY *70
+              'level': path[i].level,
+              'value': path[i].value + this.posY * 70
             });
           }
         }
@@ -52,11 +52,11 @@ class Hill {
       path = newPath;
     }
     //face ca in mijloc sa nu fie asa ascutit
-    var overrideRange = [Math.floor(path.length/2 -4) ,Math.floor(path.length/2 +4) ];
-    for (var i = overrideRange[0]; i <= overrideRange[1] ; i++) {
-      if (i != overrideRange/2){
-       
-        path[i]['value'] =randomRange(this.height - this.rangeY, this.height + this.rangeY) * 0.85;
+    var overrideRange = [Math.floor(path.length / 2 - 4), Math.floor(path.length / 2 + 4)];
+    for (var i = overrideRange[0]; i <= overrideRange[1]; i++) {
+      if (i != overrideRange / 2) {
+
+        path[i]['value'] = randomRange(this.height - this.rangeY, this.height + this.rangeY) * 0.85;
       }
     }
     return path;
@@ -64,7 +64,7 @@ class Hill {
   }
   draw(path) {
     var mountain = new Konva.Shape({
-      sceneFunc: function(context) {
+      sceneFunc: function (context) {
         context.beginPath();
         context.moveTo(0, canvasHeight);
         var sectorLength = canvasWidth / path.length;
@@ -90,9 +90,9 @@ class Hill {
     stage.add(layer);
   }
 }
-function drawSky () {
-  var blueList = ['#6ca6cd' , '#9ac0cd', '#a9d4e1','#3a9dbc','#4a90ad'];
-  var randomList = ['#f5b049','#77bfa3', '#b7e3cc','#6c5f78','#6dd472' ];
+function drawSky() {
+  var blueList = ['#6ca6cd', '#9ac0cd', '#a9d4e1', '#3a9dbc', '#4a90ad'];
+  var randomList = ['#f5b049', '#77bfa3', '#b7e3cc', '#6c5f78', '#6dd472'];
   var color1 = blueList[Math.floor(Math.random() * blueList.length)];
   var color2 = randomList[Math.floor(Math.random() * randomList.length)];
   var sky = new Konva.Rect({
@@ -100,8 +100,8 @@ function drawSky () {
     y: 0,
     width: canvasWidth,
     height: canvasHeight,
-    fillLinearGradientStartPoint: { x : canvasWidth/2, y: canvasHeight},
-    fillLinearGradientEndPoint: { x : canvasWidth/2, y :0 },
+    fillLinearGradientStartPoint: { x: canvasWidth / 2, y: canvasHeight },
+    fillLinearGradientEndPoint: { x: canvasWidth / 2, y: 0 },
     fillLinearGradientColorStops: [0, color2, 1, color1],
     strokeWidth: 4
   });
@@ -118,10 +118,10 @@ var stage = new Konva.Stage({
 // then create layer
 var layer = new Konva.Layer();
 
-var colors = [ '#2a2a2a', '#3f3f3f', '#545454' ];
+var colors = ['#2a2a2a', '#3f3f3f', '#545454'];
 
 drawSky();
-for (let i = 2; i >= 0; i--){
+for (let i = 2; i >= 0; i--) {
   let hill = new Hill(4, 250, 30, 4, colors[i], i);
   let path = hill.getPath();
   hill.draw(path);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Simple, yet Good Looking Procedural Generated Hills
+My attempt at making procedural hills and mountains. Inspired by 
+[Scott Turner's post](https://heredragonsabound.blogspot.md/2016/12/how-to-decorate-mountain-part-2.html).
+
+## **[Try it](https://cubbic.github.io/HillsAndTrees/)**
+
+My progress so far
+
+* First draw of edges
+![Hills 1](pics/hills0.png)
+
+* Added gradient sky, multiple lines of hills and painted all in pretty colors taken from [color-hex.com](http://www.color-hex.com/) 
+![Hills 2](pics/hills1.png)
